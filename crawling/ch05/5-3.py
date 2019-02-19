043e0e9de6bb510b156fe693e59aeccd8243e9cb
busan-hyh <yeonhwa001@daum.net>
2019-02-19 16:28:21 +0900

    190219 pandas dataframe

--- crawling/ch05/5-3.py
+++ crawling/ch05/5-3.py
@@ -5,11 +5,12 @@ df_exam = pd.read_csv('./data/exam.csv')
 # print(df_exam)
 
 # 데이터프레임 슬라이스
-# print(df_exam[2:4])# 2행에서 4행이전까지 표시. 필요없는 열은 그냥 빼면 되므로 ㄴㄴ
+# print(df_exam[2:4])# 2행에서 4행이전까지, 모든열 표시
+print(df_exam.loc[:,['math']])# 모든행, math열 표시
 
 # 파생변수
 df_exam['total'] = df_exam.math + df_exam.english + df_exam.science
 # print(df_exam[2:4])
 
 df_exam['mean'] = round(df_exam.total / 3)
-print(df_exam[2:4])
+# print(df_exam[2:4])
